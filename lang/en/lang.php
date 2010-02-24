c5dda38d64e3aed3582137b2c90e140f4aab9556
Andreas Gohr <andi@splitbrain.org>
2010-02-24 21:34:37 +0100

    Added word counter and localization

---
+++ lang/en/lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['js']['sortasc']   = 'Sort Ascending';
+$lang['js']['sortdesc']  = 'Sort Descending';
+$lang['js']['indent']    = 'Add Indention';
+$lang['js']['outdent']   = 'Remove Indention';
+$lang['js']['counter']   = 'Count Words';
+
+$lang['js']['notext']    = 'No text selected';
+
+$lang['js']['words']     = '%d words';
+$lang['js']['chars']     = '%d chars';
+$lang['js']['total']     = 'Total:';
+$lang['js']['section']   = 'Section:';
+$lang['js']['selection'] = 'Selection:';
+
--- script.js
+++ script.js
@@ -7,41 +7,49 @@ if(window.toolbar!=undefined){
         "list": [
             {
                 "type":  "toolbox_sort",
-                "title": "Sort ascending",
+                "title": LANG.plugins.toolbox.sortasc,
                 "icon":  DOKU_BASE+"lib/plugins/toolbox/pix/sort_ascending.png",
                 "key":   "",
                 "reverse": 0
             },
             {
                 "type":  "toolbox_sort",
-                "title": "Sort descending",
+                "title": LANG.plugins.toolbox.sortdesc,
                 "icon":  DOKU_BASE+"lib/plugins/toolbox/pix/sort_descending.png",
                 "key":   "",
                 "reverse": 1
             },
             {
                 "type":  "toolbox_indent",
-                "title": "Add Indention",
+                "title": LANG.plugins.toolbox.indent,
                 "icon":  DOKU_BASE+"lib/plugins/toolbox/pix/text_indent.png",
                 "key":   "",
                 "reverse": 0
             },
             {
                 "type":  "toolbox_indent",
-                "title": "Remove Indention",
+                "title": LANG.plugins.toolbox.outdent,
                 "icon":  DOKU_BASE+"lib/plugins/toolbox/pix/text_indent_remove.png",
                 "key":   "",
                 "reverse": 1
+            },
+            {
+                "type":  "toolbox_counter",
+                "title": LANG.plugins.toolbox.counter,
+                "icon":  DOKU_BASE+"lib/plugins/toolbox/pix/application_view_list.png",
+                "key":   "",
             }
         ]
     };
 }
 
-
+/**
+ * Sort the selected text
+ */
 function tb_toolbox_sort(btn, opts, edid){
     var selection = getSelection($(edid));
     if(!selection.getLength()){
-        alert('no text selected');
+        alert(LANG.plugins.toolbox.notext);
         return;
     }
 
@@ -55,10 +63,13 @@ function tb_toolbox_sort(btn, opts, edid){
     pickerClose();
 }
 
+/**
+ * Indent the selected text
+ */
 function tb_toolbox_indent(btn, opts, edid){
     var selection = getSelection($(edid));
     if(!selection.getLength()){
-        alert('no text selected');
+        alert(LANG.plugins.toolbox.notext);
         return;
     }
 
@@ -76,3 +87,73 @@ function tb_toolbox_indent(btn, opts, edid){
     pasteText(selection,text,{});
     pickerClose();
 }
+
+/**
+ * Count words and characters
+ *
+ * @link http://www.dokuwiki.org/tips:wordcounter
+ */
+function tb_toolbox_counter(btn, opts, edid){
+
+    function charcounter(text){
+        var list = text.split(/[^\w\-_]+/);
+        var len  = text.length;
+        if(list[len-1] == '') len--;
+        if(list[0] == '') len--;
+        if(len < 0) len=0;
+        return len;
+    }
+
+    function wordcounter(text){
+        var list = text.split(/[^\w\-_]+/);
+        var len  = list.length;
+        if(list[len-1] == '') len--;
+        if(list[0] == '') len--;
+        if(len < 0) len=0;
+        return len;
+    }
+
+
+    var obj  = $(edid);
+    var call = 0;
+    var wall = 0;
+    var csec = charcounter(obj.value);
+    var wsec = wordcounter(obj.value);
+
+    if(obj.form.elements.prefix && obj.form.elements.prefix.value){
+        call += charcounter(obj.form.elements.prefix.value);
+        wall += wordcounter(obj.form.elements.prefix.value);
+    }
+    if(obj.form.elements.suffix && obj.form.elements.suffix.value){
+        call += charcounter(obj.form.elements.suffix.value);
+        wall += wordcounter(obj.form.elements.suffix.value);
+    }
+
+    var out = '';
+
+    if(call){
+        out += LANG.plugins.toolbox.total;
+        out += "  "+LANG.plugins.toolbox.chars.replace('%d',(call + csec));
+        out += "  "+LANG.plugins.toolbox.words.replace('%d',(wall + wsec))+"\n";
+
+        out += LANG.plugins.toolbox.section;
+    }else{
+        out += LANG.plugins.toolbox.total;
+    }
+    out += "  "+LANG.plugins.toolbox.chars.replace('%d',csec);
+    out += "  "+LANG.plugins.toolbox.words.replace('%d',wsec)+"\n";
+
+    var selection = getSelection($(edid));
+    if(selection.getLength()){
+        var text = selection.getText();
+
+        out += LANG.plugins.toolbox.selection;
+        out += "  "+LANG.plugins.toolbox.chars.replace('%d',charcounter(text));
+        out += "  "+LANG.plugins.toolbox.words.replace('%d',wordcounter(text))+"\n";
+    }
+
+    pickerClose();
+    alert(out);
+}
+
+
