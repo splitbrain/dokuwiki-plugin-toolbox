70761ca8ee1cee59bf24ec034faa97134d53dfba
Andreas Gohr <andi@splitbrain.org>
2010-08-08 19:29:22 +0200

    German translation added

---
+++ lang/de/lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['js']['sortasc']   = 'Aufsteigend sortieren';
+$lang['js']['sortdesc']  = 'Absteigend sortieren';
+$lang['js']['indent']    = 'Einrückung hinzufügen';
+$lang['js']['outdent']   = 'Einrückung entfernen';
+$lang['js']['counter']   = 'Wörter zählen';
+
+$lang['js']['notext']    = 'Kein Text ausgewählt';
+
+$lang['js']['words']     = '%d Wörter';
+$lang['js']['chars']     = '%d Buchstaben';
+$lang['js']['total']     = 'Gesamt:';
+$lang['js']['section']   = 'Abschnitt:';
+$lang['js']['selection'] = 'Auswahl:';
+
