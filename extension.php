6ff27c3cc6884edc7a0b0d98650c216a315ab1c0
Andreas Gohr <andi@splitbrain.org>
2017-03-18 15:54:06 +0100

    we now build a universal web extension
    This works in Chrome and Firefox and should also work in MS Edge

--- extension.php
+++ extension.php
@@ -1,16 +1,16 @@
 #!/usr/bin/php
 <?php
 /**
- * This script builds the chrome version of the plugin
+ * This script builds the extension version of the plugin
  */
-if('cli' != php_sapi_name()) die();
-
-require("../../../inc/init.php");
+if('cli' != php_sapi_name()) die('This is a command line script only');
+if(!defined('DOKU_INC')) define('DOKU_INC', __DIR__ . '/../../../');
+require_once(DOKU_INC . 'inc/init.php');
 
 // build script
 {
     // open output
-    $fh = fopen(__DIR__ . '/chrome/script.js', 'w');
+    $fh = fopen(__DIR__ . '/extension/script.js', 'w');
 
     // load the language files
     unset($lang);
@@ -37,8 +37,24 @@ require("../../../inc/init.php");
 // set version in manifest
 {
     $info = confToHash(__DIR__ . '/plugin.info.txt');
-    $manifest = __DIR__ . '/chrome/manifest.json';
+    $manifest = __DIR__ . '/extension/manifest.json';
     $m = json_decode(file_get_contents($manifest));
     $m->version = str_replace('-', '.', $info['date']);
     file_put_contents($manifest, json_encode($m, JSON_PRETTY_PRINT));
+}
+
+// create zip file
+{
+    $filename = __DIR__ . '/extension.zip';
+    $zip = new ZipArchive();
+    if($zip->open($filename, ZipArchive::CREATE) !== true) {
+        die("cannot open <$filename>\n");
+    }
+
+    $files = glob(__DIR__ . '/extension/*');
+    foreach($files as $file) {
+        $zip->addFile($file, basename($file));
+    }
+
+    $zip->close();
 }
--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,4 @@
-/* global chrome */
+/* global chrome browser */
 
 /**
  * check if the current tab is a dokuwiki edit session and
@@ -7,15 +7,15 @@
 (function () {
     // guards to make sure we only run on DokuWiki:
     var gen = document.querySelector("meta[name='generator']");
-    if(!gen || (gen.content.search(/DokuWiki/) === -1) ) return;
+    if (!gen || (gen.content.search(/DokuWiki/) === -1)) return;
     var edi = document.querySelector("#wiki__text");
-    if(!edi) return;
+    if (!edi) return;
 
     // inject the script
     var script = document.createElement('script');
     script.setAttribute("type", "application/javascript");
-    script.src = chrome.extension.getURL('script.js');
-    script.onload = function() {
+    script.src = (chrome || browser).extension.getURL('script.js');
+    script.onload = function () {
         this.remove();
     };
     (document.head || document.documentElement).appendChild(script);
